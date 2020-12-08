2b0c5b0ea2aaee861cb5ac706fde1e10877be4f6
konathalas98 <konathalasaranya1998@gmail.com>
2020-12-08 21:03:19 +0530

    FREECRM

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,24 @@
+package runner;
+
+
+import org.junit.runner.RunWith;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+
+
+//@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "Feature"
+		,glue={"stepDefinition"}
+		,tags={"@LoginTest"}
+		//,monochrome=true //to display clear output on console in readable format
+		//,dryRun=true //checks the mapping between feature file and stepDef file
+		)
+
+public class TestRunner extends AbstractTestNGCucumberTests{
+
+}
